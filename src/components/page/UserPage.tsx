3248cb1c23655181c7dbee2a858fd685cedaeaa3
Narapat Piboonchaivanich <narapat.sitc@gmail.com>
2025-04-20 00:42:42 +0700

    Fix some UI

--- src/components/page/UserPage.tsx
+++ src/components/page/UserPage.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Banner from "../Banner";
 import Homepage from "../HomepageCard"
+import styles from "../Button.module.css";
+import { useRouter } from "next/navigation";
 
 export default function UserPage() {
+
+  const router = useRouter();
+
     return (
       <>
       
@@ -12,7 +19,7 @@ export default function UserPage() {
          <div className="flex flex-row px-6 space-x-12 pt-2 pb-9">
         <Homepage
          restaurantName="Jeh-O Chula"
-         imgSrc="/img/Food3.jpg"
+         imgSrc="/img/Food1.jpg"
          description="A small shophouse with a simple appearance that doesn't only have delicious Mama Oho, Je Oh has many other top menus, most of which are familiar Thai-Chinese comfort foods. But what makes it different is the use of quality ingredients and excellent cooking skills. Therefore, each dish is perfectly delicious. Popular menus such as crispy fried tofu with garlic, delicious duck rice soup, and Je Oh's special recipe crispy pork are really not to be missed."
         />
         
@@ -24,6 +31,20 @@ export default function UserPage() {
         
         </div>
        
+        <div className="w-full flex justify-center mb-10">
+          <button
+            className={`group ${styles["button"]}`}
+            onClick={(e) => {
+              e.stopPropagation();
+              router.push("/hotels");
+            }}
+          >
+            <span className="relative z-10 text-white group-hover:text-black transition-colors duration-300">
+              View Restaurants
+            </span>
+          </button>
+        </div>
+
        </div>
       </>
     );
