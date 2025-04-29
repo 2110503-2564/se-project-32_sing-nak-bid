1ff44011962461204b6e00c7341baa86caa26c25
tansit1 <tankhun006@gmail.com>
2025-04-29 20:10:56 +0700

    playwright done yayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

---
+++ tests/us2-4.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('admin can delete a review successfully', async ({ page }) => {
+    await page.goto('http://localhost:3000/api/auth/signin');
+    await page.fill('input[name="email"]', 'admin@gmail.com');
+    await page.fill('input[name="password"]', '12345678');
+    await page.click('button[type="submit"]');
+  
+    await page.goto('http://localhost:3000');
+
+    await page.getByRole('button', { name: /menu toggle/i }).click();
+    await page.getByText('Review Verify').click()
+
+    await page.waitForLoadState('networkidle');
+
+    await page.getByRole('heading', { name: /Jeh-O Chula/i }).click();
+    await page.locator('button[title="Delete Review"]').first().click();
+    await page.getByRole('button', { name: /Confirm/i }).click();
+
+    await expect(page.locator('text=Review successfully deleted!')).toBeVisible();
+
+});
